14a2ab2211e5075fe803437e329e9f5b552dc640
Valentina Dinkova <vdinkova@fmi.uni-sofia.bg>
2011-05-11 00:15:55 +0300

    HW6

--- HW6/templates_c/134698f94fc70d4dc67bb071fd3c80b5e24d4616.file.index.tpl.php
+++ HW6/templates_c/134698f94fc70d4dc67bb071fd3c80b5e24d4616.file.index.tpl.php
@@ -1,17 +1,17 @@
-<?php /* Smarty version Smarty-3.0.7, created on 2011-05-10 20:32:18
+<?php /* Smarty version Smarty-3.0.7, created on 2011-05-10 23:13:23
          compiled from "templates_c/index.tpl" */ ?>
-<?php /*%%SmartyHeaderCode:7955214334dc97ac895f537-68639443%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+<?php /*%%SmartyHeaderCode:11342687354dc9aa733a3527-81933015%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
   array (
     '134698f94fc70d4dc67bb071fd3c80b5e24d4616' => 
     array (
       0 => 'templates_c/index.tpl',
-      1 => 1305052229,
+      1 => 1305062000,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '7955214334dc97ac895f537-68639443',
+  'nocache_hash' => '11342687354dc9aa733a3527-81933015',
   'function' => 
   array (
   ),
@@ -21,11 +21,11 @@ $_smarty_tpl->decodeProperties(array (
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="bg" lang="bg">
   <head>
-    <title><?php echo $_smarty_tpl->getVariable('title')->value;?>
+    <title><?php echo (($tmp = @$_smarty_tpl->getVariable('title')->value)===null||$tmp==='' ? 'no title' : $tmp);?>
 </title>
     <meta http-equiv="content-type" content="text/html;charset=utf-8" />
     <?php  $_smarty_tpl->tpl_vars['j'] = new Smarty_Variable;
- $_from = $_smarty_tpl->getVariable('js')->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+ $_from = (($tmp = @$_smarty_tpl->getVariable('js')->value)===null||$tmp==='' ? array() : $tmp); if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
 if ($_smarty_tpl->_count($_from) > 0){
     foreach ($_from as $_smarty_tpl->tpl_vars['j']->key => $_smarty_tpl->tpl_vars['j']->value){
 ?>
--- HW6/templates_c/index.tpl
+++ HW6/templates_c/index.tpl
@@ -2,21 +2,21 @@
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="bg" lang="bg">
   <head>
-    <title>{$title}</title>
+    <title>{$title|default:'no title'}</title>
     <meta http-equiv="content-type" content="text/html;charset=utf-8" />
-    {foreach from=$js item=j}
+    {foreach from=$js|default:[] item=j}
       <link rel="stylesheet" href="{$jsFolder}/{$j}" type="text/css" media="screen" />
     {/foreach} 
-    {foreach from=$css item=c}
+    {foreach from=$css|default:[] item=c}
       <script src="{$cssFolder}/{$c}" type="text/javascript" language="javascript" charset="utf-8"></script>
     {/foreach} 
   </head>
   <body>
   <p>
-    {$message}
+    {$message|default:""}
   </p>
   <p>
-    {$message2}
+    {$message2|default:""}
   </p>
   </body>
 </html>
