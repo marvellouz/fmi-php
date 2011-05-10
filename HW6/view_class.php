71c32e6fec0d76d7d58ed89d41e5e76e342a5ef5
Valentina Dinkova <vdinkova@fmi.uni-sofia.bg>
2011-05-10 22:48:39 +0300

    HW6

--- HW6/view_class.php
+++ HW6/view_class.php
@@ -4,57 +4,37 @@ require_once("./libs/Smarty.class.php");
 
 class View implements IView {
   private $smarty;
-  private $title;
-  private $jsFolder;
-  private $js;
-  private $cssFolder;
-  private $css;
-  private $varNameValue;
 
   function __construct() {
     $this->smarty = new Smarty();
   }
 
   public function display($pageName) {
-    $this->smarty->assign('title', $this->title);
-    $this->smarty->assign('jsFolder', $this->jsFolder);
-    $this->smarty->assign('js', $this->js);
-    $this->smarty->assign('cssFolder', $this->cssFolder);
-    $this->smarty->assign('css', $this->css);
-
-    foreach($this->varNameValue as $varName => $varValue) {
-      $this->smarty->assign($varName, $varValue);
-    }
-
     $this->smarty->display($pageName);
   }
 
   public function assignTemplateVariable($varName, $varValue) {
-    $this->varNameValue[$varName] = $varValue;
+    $this->smarty->assign($varName, $varValue);
   }
 
   public function setPageTitle($title) {
-    $this->title = $title;
+    $this->smarty->assign('title', $title);
   }
 
   public function setJavascriptFolder($jsFolder) {
-    $this->jsFolder = $jsFolder;
+    $this->smarty->assign('jsFolder', $jsFolder);
   }
 
   public function addJavascriptFiles($js /* array */) {
-    foreach ($js as $j) {
-      $this->js[] = $j;
-    }
+    $this->smarty->assign('js', $js);
   }
   
   public function setCSSFolder($cssFolder) {
-    $this->cssFolder=$cssFolder;
+    $this->smarty->assign('cssFolder', $cssFolder);
   }
 
   public function addCSSFiles($css /* array */) {
-    foreach ($css as $c) {
-      $this->css[] = $c;
-    }
+    $this->smarty->assign('css', $css);
   }
 
 }
